51530ea05b59a9f4492fd41f1a1c3513638a25e3
fhnaseer <fhnaseer@live.com>
2019-08-19 15:36:37 +0200

    Moving app component to app folder,

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
